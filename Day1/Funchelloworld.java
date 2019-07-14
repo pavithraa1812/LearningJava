510601bbc9b161ea2943bb9c63de948d75372597
Pavithraa_18 <37111537+pavithraa1812@users.noreply.github.com>
2019-07-14 18:42:03 +0530

    Add files via upload

---
+++ Day1/Funchelloworld.java
@@ -0,0 +1,11 @@
+public class Funchelloworld
+{
+    static void function()
+    {
+        System.out.println("Hello World");
+    }
+    public static void main(String[]srgs)
+    {
+        function();
+    }
+}
